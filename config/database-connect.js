ae5054a68abde9a40e3e4dabd5de596b8fe81405
DukTai <nguyenductai.12a2tqk@gmail.com>
2025-06-24 17:17:13 +0700

    Tạm commit thay đổi trong package.json

--- config/database-connect.js
+++ config/database-connect.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = process.env.DB_PORT || 5432;
 
-const sequelize = new Sequelize("postgres", "postgres", "123", {
+const sequelize = new Sequelize("postgres", "postgres", "123456", {
   host: DB_HOST,
   port: DB_PORT,
   dialect: "postgres",
