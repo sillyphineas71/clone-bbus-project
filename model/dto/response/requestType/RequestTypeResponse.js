636024fd21254eb53d73914bda48cb19e3f90526
DukTai <nguyenductai.12a2tqk@gmail.com>
2025-07-25 12:12:43 +0700

    Done route and requestType Controller

---
+++ model/dto/response/requestType/RequestTypeResponse.js
@@ -0,0 +1,7 @@
+class RequestTypeResponse {
+  constructor(requestTypeId, requestTypeName) {
+    this.requestTypeId = requestTypeId;
+    this.requestTypeName = requestTypeName;
+  }
+}
+module.exports = RequestTypeResponse;
